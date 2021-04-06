f790019bbb93605b185a15795ec754d1f85ac175
Joost <j.w.bambacht@student.tudelft.nl>
2021-04-06 15:09:39 +0200

    remove redundant files

--- cryptfolio-react/src/constants.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-//API URL
-export const API_URL = '/CryptFolio/';
-
-//API End Points
-export const LOGIN = `${API_URL}/auth/login`;
-export const REGISTER = `${API_URL}/auth/register`;
-export const FORGOT_PASSWORD = `${API_URL}/auth/reset`;
